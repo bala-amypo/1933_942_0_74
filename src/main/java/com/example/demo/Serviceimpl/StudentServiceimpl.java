9cbcb9b591c7a48c0d78ea8265b687ab402b177d
bala-amypo <bala@amypo.in>
2025-12-17 09:44:33 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/Serviceimpl/StudentServiceimpl.java
@@ -2,9 +2,16 @@ package com.example.demo.Serviceimpl;
 
 package com.example.demo.service.stereotype.Service;
 
-import com.example.demo.service.StudentService
+import com.example.demo.service.StudentService;
 
 @Service
 public class StudentServiceimpl implements StudentService{
 
+private final StuRepository studentrepository;
+public StudentServiceimpl(StuRepository studentrepository){
+    this.studentrepository=studentrepository;
+}
+public Stuentity saveStudent(Stuentity student){
+return studentrepository    
+}
 }
--- src/main/java/com/example/demo/repository/StuRepository.java
+++ src/main/java/com/example/demo/repository/StuRepository.java
@@ -6,7 +6,4 @@ import com.example.demo.entity.Stuentity;
 @Repository
 public interface StuRepository extends JpaRepository<Stuentity,Long>{
 
-Stuentity saveStudent(Stuentity student){
-    
-}
 }
